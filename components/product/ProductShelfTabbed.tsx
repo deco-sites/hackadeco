a8f6d0061d5d5c5f82be2877b1bc448142d06b19
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2023-11-03 12:56:16 -0300

    atualizado Cards Largos, foi adicionado props para maior controle via admin

--- components/product/ProductShelfTabbed.tsx
+++ components/product/ProductShelfTabbed.tsx
@@ -7,36 +7,43 @@ import Header from "$store/components/ui/SectionHeader.tsx";
 import Slider from "$store/components/ui/Slider.tsx";
 import SliderJS from "$store/islands/SliderJS.tsx";
 import { useId } from "$store/sdk/useId.ts";
-import LargeProductCard from "./LargeProductCard.tsx";
+import LargeProductCard, { Props as ILargeCard } from "./LargeProductCard.tsx";
 
-function TabbedProductShelf() {
+interface Props {
+  largeCards?: ILargeCard[]
+}
+
+function TabbedProductShelf({largeCards}: Props) {
   const id = useId();
-  
-  const products = [
+
+  const cardsExamples = [
     {
-      name: "Casacos e Jaquetas",
-      discount: 60,
+      title: "Casacos e Jaquetas",
+      subtitle: "ATÉ 60% OFF",
+      cta: "Veja Mais",
       imageUrl: "https://static.netshoes.com.br/produtos/jaqueta-black-river-puffer-lisa-masculina/06/MCN-0089-006/MCN-0089-006_zoom1.jpg?ts=1695612270&ims=544x"
     },
     {
-      name: "Shorts de Academia",
-      discount: 40,
-      imageUrl:
-        "https://static.netshoes.com.br/produtos/short-adidas-marathon-20-feminino/06/COL-5376-006/COL-5376-006_zoom1.jpg?ts=1634268808&ims=544x"
+      title: "Shorts de Academia",
+      subtitle: "ATÉ 40% OFF",
+      cta: "Veja Mais",
+      imageUrl: "https://static.netshoes.com.br/produtos/short-adidas-marathon-20-feminino/06/COL-5376-006/COL-5376-006_zoom1.jpg?ts=1634268808&ims=544x"
     },
     {
-      name: "Bonés Esportivos",
-      discount: 60,
-      imageUrl:
-        "https://static.netshoes.com.br/produtos/bone-nike-aba-curva-featherlight-run/06/HZM-0082-006/HZM-0082-006_zoom1.jpg?ts=1698041911&ims=544x"
+      title: "Bonés Esportivos",
+      subtitle: "ATÉ 60% OFF",
+      cta: "Veja Mais",
+      imageUrl: "https://static.netshoes.com.br/produtos/bone-nike-aba-curva-featherlight-run/06/HZM-0082-006/HZM-0082-006_zoom1.jpg?ts=1698041911&ims=544x"
     },
     {
-      name: "Tênis de Corrida",
-      discount: 30,
-      imageUrl:
-        "https://static.netshoes.com.br/produtos/tenis-de-corrida-feminino-mizuno-wave-rider-26-roxy/18/2FU-8123-018/2FU-8123-018_zoom1.jpg?ts=1684848109&ims=544x"
+      title: "Tênis de Corrida",
+      subtitle: "ATÉ 60% OFF",
+      cta: "Veja Mais",
+      imageUrl: "https://static.netshoes.com.br/produtos/tenis-de-corrida-feminino-mizuno-wave-rider-26-roxy/18/2FU-8123-018/2FU-8123-018_zoom1.jpg?ts=1684848109&ims=544x"
     }
   ];
+
+  const cards = largeCards ? (largeCards.length > 0 ? largeCards : cardsExamples) : cardsExamples;
 
   return (
     <div class="w-full container  py-8 flex flex-col gap-8 lg:gap-12 lg:py-10">
@@ -46,15 +53,16 @@ function TabbedProductShelf() {
         class="container grid grid-cols-[48px_1fr_48px] px-0 sm:px-5"
       >
         <Slider class="carousel carousel-center sm:carousel-end gap-6 col-span-full row-start-2 row-end-5">
-          {products?.map((prd, index) => (
+          {cards?.map((card, index) => (
             <Slider.Item
               index={index}
               class="carousel-item first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0"
             >
               <LargeProductCard
-                name={prd.name}
-                discount={prd.discount}
-                imageUrl={prd.imageUrl}
+                title={card.title}
+                subtitle={card.subtitle}
+                cta={card.cta}
+                imageUrl={card.imageUrl}
               />
             </Slider.Item>
           ))}
