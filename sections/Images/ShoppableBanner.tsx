b7048d1c0c6978e74afe32b2da635fe3bc77bec2
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2023-11-04 10:08:34 -0300

    corrigido admin

--- sections/Images/ShoppableBanner.tsx
+++ sections/Images/ShoppableBanner.tsx
@@ -48,36 +48,64 @@ export default function ShoppableBanner(props: Props) {
   const { link, text, title, image, pins } = { ...DEFAULT_PROPS, ...props };
 
   return (
-    <div class="container">
+    <div class="container py-20">
       <div class="card lg:card-side rounded grid grid-cols-1 lg:grid-cols-[70%_30%]">
         <figure class="relative rounded-lg">
           <Picture>
             <Source
               media="(max-width: 767px)"
-              src="https://img.freepik.com/premium-photo/active-sport-girl-runner-running-yellow-background_545934-6630.jpg"
+              src={image?.mobile}
               width={150}
               height={150}
             />
             <Source
               media="(min-width: 768px)"
-              src="https://img.freepik.com/premium-photo/active-sport-girl-runner-running-yellow-background_545934-6630.jpg"
+              src={image?.desktop ? image?.desktop : image?.mobile}
               width={384}
               height={227}
             />
             <img
               class="w-full object-cover"
               sizes="(max-width: 640px) 100vw, 30vw"
-              src="https://img.freepik.com/premium-photo/active-sport-girl-runner-running-yellow-background_545934-6630.jpg"
+              src={image?.mobile}
               alt={image?.altText}
               decoding="async"
               loading="lazy"
             />
           </Picture>
-          
+          {pins.map(({ mobile, desktop, link, label }) => (
+            <>
+              <a
+                href={link}
+                class="absolute w-min btn btn-accent rounded-full hover:rounded text-accent no-animation md:scale-[30%] hover:text-accent-content hover:scale-125 sm:hidden"
+                style={{
+                  left: `${mobile.x}%`,
+                  top: `${mobile.y}%`,
+                }}
+              >
+                <span>{label}</span>
+              </a>
+              <a
+                href={link}
+                class="absolute w-min btn btn-accent rounded-full hover:rounded text-accent no-animation md:scale-[30%] hover:text-accent-content hover:scale-125 hidden sm:inline-flex"
+                style={{
+                  left: `${desktop?.x ?? mobile.x}%`,
+                  top: `${desktop?.y ?? mobile.y}%`,
+                }}
+              >
+                <span>{label}</span>
+              </a>
+            </>
+          ))}
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Sobre a Tenshoes</h2>
-          <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with</p>
+          <h2 class="card-title">{title}</h2>
+          <p>
+            {text}
+          </p>
+          <div class="card-actions justify-end">
+            <a class="btn bg-zinc-700 text-zinc-50" href={link?.href}>{link?.text}</a>
+          </div>
         </div>
       </div>
     </div>
